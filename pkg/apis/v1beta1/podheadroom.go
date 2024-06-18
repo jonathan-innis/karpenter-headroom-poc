fc2944e4c573b595c33c87a0489cbcdfb9bb1fae
Jonathan Innis <joinnis@amazon.com>
2024-06-18 15:57:12 -0700

    Rename Headroom to PodHeadroom

--- pkg/apis/v1beta1/podheadroom.go
+++ pkg/apis/v1beta1/podheadroom.go
@@ -22,45 +22,45 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// Headroom is the Schema for the Headrooms API
+// PodHeadroom is the Schema for the PodHeadrooms API
 // +kubebuilder:object:root=true
-// +kubebuilder:resource:path=headrooms,scope=Namespace,categories=karpenter
+// +kubebuilder:resource:path=podheadrooms,scope=Namespace,categories=karpenter
 // +kubebuilder:subresource:status,scale
-type Headroom struct {
+type PodHeadroom struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	// +required
-	Spec   HeadroomSpec   `json:"spec"`
-	Status HeadroomStatus `json:"status,omitempty"`
+	Spec   PodHeadroomSpec   `json:"spec"`
+	Status PodHeadroomStatus `json:"status,omitempty"`
 }
 
-// HeadroomList contains a list of Headrooms
+// PodHeadroomList contains a list of PodHeadrooms
 // +kubebuilder:object:root=true
-type HeadroomList struct {
+type PodHeadroomList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []Headroom `json:"items"`
+	Items           []PodHeadroom `json:"items"`
 }
 
-type HeadroomSpec struct {
-	// SatisfiedBy allows you to specify a selector against any pods that this Headroom should match against
-	// Matching against pods with SatisfiedBy implies that the scheduling simulation will fill-up the headroom with pod
-	// capacity as it schedules. e.g. Headroom for 500Mi memory and Pods that match that have 300Mi memory means the scheduling
-	// simulation will only consider 200Mi memory for the headroom
+type PodHeadroomSpec struct {
+	// SatisfiedBy allows you to specify a selector against any pods that this PodHeadroom should match against
+	// Matching against pods with SatisfiedBy implies that the scheduling simulation will fill-up the pod headroom with pod
+	// capacity as it schedules. e.g. PodHeadroom for 500Mi memory and Pods that match that have 300Mi memory means the scheduling
+	// simulation will only consider 200Mi memory for the pod headroom
 	// +optional
 	SatisfiedBy PodSelector `json:"satisfiedBy,omitempty"`
-	// Template is a Pod Template to use for Headroom when scheduling
+	// Template is a Pod Template to use for PodHeadroom when scheduling
 	// +optional
 	Template v1.PodTemplateSpec `json:"template"`
-	// Replicas is the number of pods to model from the template for Headroom during scheduling
+	// Replicas is the number of pods to model from the template for PodHeadroom during scheduling
 	// +optional
 	Replicas int32 `json:"replicas,omitempty"`
 }
 
-type HeadroomStatus struct {
+type PodHeadroomStatus struct {
 	// Conditions contains signals for health and readiness
-	// It contains a status condition that indicates whether the capacity Headroom
+	// It contains a status condition that indicates whether the capacity PodHeadroom
 	// is available on the cluster, allowing external components the ability to know
 	// when all the capacity exists
 	// +optional
